06a2f10274816d750cd1d7c24b95684b4b502c43
Ray Donnelly <mingw.android@gmail.com>
2021-01-14 15:32:56 +0100

    macOS builds ok

--- recipe/build-base.sh
+++ recipe/build-base.sh
@@ -3,5 +3,13 @@ set -ex
 
 # install C lib
 export PREFIX="${PREFIX}"
-./make.sh
+if [[ ${target_platform} =~ osx-* ]]; then
+  # The build fails when you set this. Kernel headers seem in-conflict with
+  # user-space headers (or capstone headers/macros)
+  # export CAPSTONE_HAS_OSXKERNEL=yes
+  # .. we do use a similarly named variable in a patch though:
+  export CAPSTONE_HAS_OSXKERNEL_CONDA=yes
+  # .. we also elect not to pass macos-universal-no as the target
+  export SDKROOT=${CONDA_BUILD_SYSROOT}
+fi
 ./make.sh install
